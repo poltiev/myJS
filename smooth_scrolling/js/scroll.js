e814261da8ed3a559e061e6a82b1b94b6c8aa1ac
poltiev <poltiev@yandex.ru>
2018-11-28 15:20:23 +0300

    finish1

---
+++ smooth_scrolling/js/scroll.js
@@ -0,0 +1,8 @@
+// плавный scroll
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+	$page.animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 800);
+	return false;
+});
