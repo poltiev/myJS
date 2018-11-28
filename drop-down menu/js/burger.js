4c1c9e629164aa7d3b5f643bf921267b66e65339
poltiev <poltiev@yandex.ru>
2018-11-28 18:44:27 +0300

    burger

---
+++ drop-down menu/js/burger.js
@@ -0,0 +1,16 @@
+$(function(burger) {
+    // В класс "page" обернуто все содержимое страницы.
+    // Можно повесить событие и на body, но тогда это меню не будет работать на I-pad
+    $('.all').click(function( event ){
+        // проверяем находится ли элемент на который кликнули в нашем меню,
+        // то есть в контейнере "menu-container"
+        var eventInMenu = burger( event.target ).parents('.menu-container');
+        // если родителя нет, значит клик был вне меню и меню сворачиваем
+        if( !eventInMenu.length ){
+            burger('.main-menu').slideUp('800');
+        }
+    });
+    $('.menu-container').click(function(){
+        burger('.main-menu').slideToggle('800');
+    });
+});
